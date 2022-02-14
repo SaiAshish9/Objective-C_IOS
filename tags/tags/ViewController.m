5dac8ca3f6cb107bcb64d34c42fb9c85012ea925
saiashish9 <saiashish3760@gmail.com>
2022-02-14 13:33:47 +0530

    k

--- tags/tags/ViewController.m
+++ tags/tags/ViewController.m
@@ -15,7 +15,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _outlbl.textColor = [UIColor orangeColor];
+    _outlbl.text = @"Outlet label";
+    
+    UILabel *lbl = (UILabel *)[self.view viewWithTag:1];
+    lbl.text=@"Tag";
+    lbl.textColor = [UIColor blueColor];
+//   UIButton *x = (UIButton *)[self.view viewWithTag:1];
+//   UIImageView
+//   UITextField
 }
 
 
