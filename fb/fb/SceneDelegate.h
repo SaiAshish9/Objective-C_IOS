67c36e7d862508f44a818f3d0cdf5c4c3b55dc4f
saiashish9 <saiashish3760@gmail.com>
2022-02-14 11:39:34 +0530

    g_signin updated

---
+++ fb/fb/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  fb
+//
+//  Created by Sai Ashish Darapureddy on 14/02/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ fb/fb/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  fb
+//
+//  Created by Sai Ashish Darapureddy on 14/02/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
