cccc8bd4d762b741caf2eef8c02600bc4222b43b
saiashish9 <saiashish3760@gmail.com>
2022-02-14 13:24:07 +0530

    validation updated

--- API/API/ViewController.h
+++ API/API/ViewController.h
@@ -6,6 +6,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#define REGEX_USER_NAME_LIMIT @"^.{3,10}"
+
+// -(void) viewDidLoad(){
+//  [self setup];
+// }
+
+// -(void) setuo {
+// [txt addRegex:REGEX_USER_NAME_LIMIT] withMsg:@"Username must contain between 3 to 10 characters";
+// }
 
 @interface ViewController : UIViewController
 
--- API/API/ViewController.m
+++ API/API/ViewController.m
@@ -15,7 +15,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSURLSessionConfiguration *defaultSessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration:defaultSessionConfiguration];
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users"];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+
+    NSString *postParams = @"";
+//    NSData *postData = [postParams dataUsingEncoding:NSUTF8StringEncoding];
+    [urlRequest setHTTPMethod:@"GET"];
+//    [urlRequest setHTTPBody:postData];
+    NSURLSessionDataTask *dataTask = [defaultSession dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if(data != nil)
+        NSLog(@"Response %@",data);
+        
+        NSDictionary *results = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+        //JSON Parsing....
+        NSLog(@"Message %@",results);
+//        NSDictionary *results = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+//        NSString *message = results[@"Message"];
+    }];
+    [dataTask resume];
 }
 
 
